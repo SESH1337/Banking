693a9f93cc0b6aee40dea756647aa042810a0147
Ningendo12 <116316808+Ningendo12@users.noreply.github.com>
2025-02-02 16:15:20 +0400

    commit data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,21 +1,33 @@
+import { type } from 'os'
 import React from 'react'
 import PostCard from '../_components/postCard/postCard'
 
-function BlogPage() {
+type Post = {
+  id: number
+  title: string
+  body: string
+}
+
+const getData = async (): Promise<Post[]> => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+  if (!res.ok) {
+    throw new Error('Something went wrong...')
+  }
+
+  return res.json()
+}
+
+const BlogPage = async (): Promise<JSX.Element> => {
+  const posts = await getData()
+
   return (
-    <div className="flex gap-[20px] flex-wrap">
-      <div className="w-[30%] md:w-[45%] sm:w-full">
-        <PostCard />
-      </div>
-      <div className="w-[30%] md:w-[45%] sm:w-full">
-        <PostCard />
-      </div>
-      <div className="w-[30%] md:w-[45%] sm:w-full">
-        <PostCard />
-      </div>
-      <div className="w-[30%] md:w-[45%] sm:w-full">
-        <PostCard />
-      </div>
+    <div className="flex gap-5 flex-wrap">
+      {posts.map((post) => (
+        <div key={post.id} className="w-[30%] md:w-[45%] sm:w-full">
+          <PostCard />
+        </div>
+      ))}
     </div>
   )
 }
