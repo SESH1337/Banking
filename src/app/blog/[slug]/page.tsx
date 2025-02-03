20b94299513275709d618a13d7fb92664fc0a0d3
SESH1337 <giorgi.oniani13b@geolab.edu.ge>
2025-02-03 15:06:08 +0400

    slug add, style correction

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,40 +1,35 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './singlePost.module.css'
-import { imageConfigDefault } from 'next/dist/shared/lib/image-config'
-
+import React from 'react';
+import Image from 'next/image';
+import styles from './singlePost.module.css';
+import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
 
 export type Post = {
-  id: number
-  title: string
-  body: string
-}
+  id: number;
+  title: string;
+  body: string;
+};
 
-
-const getData = async (slug:string): Promise<Post[]> => {
-
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+const getData = async (slug: string) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
 
   if (!res.ok) {
-    throw new Error('Something went wrong...')
+    throw new Error('Something went wrong...');
   }
 
-  return res.json()
-}
-
+  return res.json();
+};
 
 type SinglePostPageProps = {
   params: {
-    slug: string
-  }
-}
-
-
+    slug: string;
+  };
+};
 
 const SinglePostPage = async ({ params }: SinglePostPageProps) => {
-  console.log(params)
-  const { slug } = params
-  const post = await getData(slug)
+
+  const { slug } = params;
+  const post = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
@@ -42,7 +37,7 @@ const SinglePostPage = async ({ params }: SinglePostPageProps) => {
       </div>
 
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image
             className={styles.avatar}
@@ -62,12 +57,10 @@ const SinglePostPage = async ({ params }: SinglePostPageProps) => {
           </div>
         </div>
 
-        <div className="flex gap-[100px]">
-          {/* {post.body} */}
-        </div>
+        <div className="flex gap-[100px]">{post.body}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SinglePostPage
+export default SinglePostPage;
